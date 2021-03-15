93bca33ce205ead9d018bb52cba8947ecf4d416d
Kalin Jack <kalinj1997@gmail.com>
2021-03-15 11:24:05 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Kalin Jack';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, to flatbook Kalin!'
